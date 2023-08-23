90b275791235adc662a2e906c53d814f8543128f
Rodrigo Silva <rodrigo.92.srb@gmail.com>
2023-08-23 18:25:51 -0300

    Starting API development

---
+++ src/main/java/rodrigosrb/myfirstapi/MyFirstApiApplication.java
@@ -0,0 +1,13 @@
+package rodrigosrb.myfirstapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstApiApplication.class, args);
+	}
+
+}
